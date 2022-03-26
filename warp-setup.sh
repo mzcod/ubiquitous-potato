f7bbe566cdb310faddcd2799f27ee3a4b44f4cab
Teodora <96918706+mzcod@users.noreply.github.com>
2022-03-26 13:13:01 -0400

    Create warp-setup.sh

---
+++ warp-setup.sh
@@ -0,0 +1,19 @@
+warp-cli register
+warp-cli remove-excluded-route 10.0.0.0/8
+warp-cli remove-excluded-route 100.64.0.0/10
+warp-cli remove-excluded-route 169.254.0.0/16
+warp-cli remove-excluded-route 172.16.0.0/12
+warp-cli remove-excluded-route 192.0.0.0/24
+warp-cli remove-excluded-route 192.168.0.0/16
+warp-cli remove-excluded-route 224.0.0.0/24
+warp-cli remove-excluded-route 240.0.0.0/4
+warp-cli remove-excluded-route 255.255.255.255/32
+warp-cli remove-excluded-route fe80::/10
+warp-cli remove-excluded-route fd00::/8
+warp-cli remove-excluded-route ff01::/16
+warp-cli remove-excluded-route ff02::/16
+warp-cli remove-excluded-route ff03::/16
+warp-cli remove-excluded-route ff04::/16
+warp-cli remove-excluded-route ff05::/16
+warp-cli add-excluded-route 147.182.158.74
+warp-cli connect
